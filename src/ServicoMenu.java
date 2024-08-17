87e4caef6de72da35db656d24709b5153844912a
Samuel <samuelsoares.academic@gmail.com>
2024-08-17 02:48:51 -0300

    Eviando toda atualização do codigo

---
+++ src/ServicoMenu.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+
+public class ServicoMenu {
+
+    //--------------- Escolhas do(s) menu(s) para o JOptionPane ---------------//
+
+    // >> Menu principal
+    private static String[] escolhaPrincipal = {"Tarefas", "Projetos", "Consultas", "Sair"};
+    // >> Menu tarefas
+    private static String[] escolhaTarefas = {"Criar tarefa", "Visualizar", "Sair"};
+    // >> Menu projetos
+    private static String[] escolhaProjetos = {"Criar projeto", "Visualizar", "Sair"};
+     
+
+
+    //----------------------------------------- Menu principal -----------------------------------------//
+    public void menuPrincipal(){
+
+        // Variável para a estrutura de repetição
+        boolean sairMenuPrincipal = false;
+
+        // Repetir programa até que a escolha sair seja executada 
+        while(sairMenuPrincipal == false){
+
+            // Escolha do Menu principal
+            int resultadoEscolhaPrincipal = JOptionPane.showOptionDialog(null, "Gerenciamento de tarefas e projetos: ", "Menu Principal", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, escolhaPrincipal, escolhaPrincipal[0]);
+        
+            switch (resultadoEscolhaPrincipal + 1) {
+                case 1:
+                    menuTarefas();
+                    break;
+            
+                case 2:
+                    menuProjetos();
+                    break;
+                
+                case 3:
+
+                    break;
+
+                case 4:
+                    sairMenuPrincipal = true;
+                    break;
+
+            }
+        }
+
+    }
+
+    //----------------------------------------- Menu(s) secundários -----------------------------------------//
+    public void menuTarefas(){
+
+        // Variável para a estrutura de repetição
+        boolean sairMenuTarefas = false;
+
+        // Repetir programa até que a escolha sair seja executada 
+        while(sairMenuTarefas == false){
+
+            // Escolha do Menu principal
+            int resultadoEscolhaTarefas = JOptionPane.showOptionDialog(null, "Gerenciamento de tarefas: ", "Menu tarefas", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, escolhaTarefas, escolhaTarefas[0]);
+        
+            switch (resultadoEscolhaTarefas + 1) {
+                case 1:
+                    
+                    break;
+            
+                case 2:
+
+                    break;
+                
+                case 3:
+                    sairMenuTarefas = true;
+                    break;
+                
+            }
+        }
+
+    }
+
+
+    public void menuProjetos(){
+
+        // Variável para a estrutura de repetição
+        boolean sairMenuProjetos = false;
+
+        // Repetir programa até que a escolha sair seja executada 
+        while(sairMenuProjetos == false){
+
+            // Escolha do Menu principal
+            int resultadoEscolhaProjetos = JOptionPane.showOptionDialog(null, "Gerenciamento de projetos: ", "Menu projetos", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, escolhaProjetos, escolhaProjetos[0]);
+        
+            switch (resultadoEscolhaProjetos + 1) {
+                case 1:
+                    
+                    break;
+            
+                case 2:
+
+                    break;
+                
+                case 3:
+                    sairMenuProjetos = true;
+                    break;
+                    
+            }
+        }
+
+    }
+
+}
+
